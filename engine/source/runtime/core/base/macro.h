390c59201de46c80866abfcf103641626492546e
cleanwaterylx <109544465+cleanwaterylx@users.noreply.github.com>
2024-01-01 19:55:58 +0800

    base

--- engine/source/runtime/core/base/macro.h
+++ engine/source/runtime/core/base/macro.h
@@ -1,64 +1,31 @@
 #pragma once
 
-#include <spdlog/spdlog.h>
+#include "runtime/core/log/log_system.h"
 
-#include <cstdint>
-#include <stdexcept>
+#include "runtime/function/global/global_context.h"
 
-namespace Compass
-{
+#include <chrono>
+#include <thread>
 
-    class LogSystem final
-    {
-    public:
-        enum class LogLevel : uint8_t
-        {
-            debug,
-            info,
-            warn,
-            error,
-            fatal
-        };
+#define LOG_HELPER(LOG_LEVEL, ...) \
+    g_runtime_global_context.m_logger_system->log(LOG_LEVEL, "[" + std::string(__FUNCTION__) + "] " + __VA_ARGS__);
 
-    public:
-        LogSystem();
-        ~LogSystem();
+#define LOG_DEBUG(...) LOG_HELPER(LogSystem::LogLevel::debug, __VA_ARGS__);
 
-        template<typename... TARGS>
-        void log(LogLevel level, TARGS&&... args)
-        {
-            switch (level)
-            {
-                case LogLevel::debug:
-                    m_logger->debug(std::forward<TARGS>(args)...);
-                    break;
-                case LogLevel::info:
-                    m_logger->info(std::forward<TARGS>(args)...);
-                    break;
-                case LogLevel::warn:
-                    m_logger->warn(std::forward<TARGS>(args)...);
-                    break;
-                case LogLevel::error:
-                    m_logger->error(std::forward<TARGS>(args)...);
-                    break;
-                case LogLevel::fatal:
-                    m_logger->critical(std::forward<TARGS>(args)...);
-                    fatalCallback(std::forward<TARGS>(args)...);
-                    break;
-                default:
-                    break;
-            }
-        }
+#define LOG_INFO(...) LOG_HELPER(LogSystem::LogLevel::info, __VA_ARGS__);
 
-        template<typename... TARGS>
-        void fatalCallback(TARGS&&... args)
-        {
-            const std::string format_str = fmt::format(std::forward<TARGS>(args)...);
-            throw std::runtime_error(format_str);
-        }
+#define LOG_WARN(...) LOG_HELPER(LogSystem::LogLevel::warn, __VA_ARGS__);
 
-    private:
-        std::shared_ptr<spdlog::logger> m_logger;
-    };
+#define LOG_ERROR(...) LOG_HELPER(LogSystem::LogLevel::error, __VA_ARGS__);
 
-} // namespace Piccolo
+#define LOG_FATAL(...) LOG_HELPER(LogSystem::LogLevel::fatal, __VA_ARGS__);
+
+#define PolitSleep(_ms) std::this_thread::sleep_for(std::chrono::milliseconds(_ms));
+
+#define PolitNameOf(name) #name
+
+#ifdef NDEBUG
+#define ASSERT(statement)
+#else
+#define ASSERT(statement) assert(statement)
+#endif
